84c54395b9f1c2f2418ef10629be9da508e54d7b
Ajitomi, Daisuke <ajitomi@gmail.com>
2021-05-01 13:22:26 +0900

    Add test for utils.

---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+"""
+Tests for utils
+"""
+import pytest
+
+from cwt.utils import i2osp, uint_to_bytes
+
+
+class TestUtils:
+    """
+    Tests for utils.
+    """
+
+    def test_utils_i2osp_invalid_arg(self):
+        with pytest.raises(ValueError) as err:
+            i2osp(270, 1)
+            pytest.fail("i2osp should fail.")
+        assert "integer too large" in str(err.value)
+
+    def test_utils_uint_to_bytes_invalid_arg(self):
+        with pytest.raises(ValueError) as err:
+            uint_to_bytes(-1)
+            pytest.fail("uint_to_bytes should fail.")
+        assert "Not a positive number." in str(err.value)
