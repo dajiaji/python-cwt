1f63639cc3e55ab4d02b7cf6fc645cdbb1ea8e08
Ajitomi Daisuke <dajiaji@gmail.com>
2023-03-26 03:34:25 +0900

    Add interop testing with t_cose.

--- tests/test_cose_hpke.py
+++ tests/test_cose_hpke.py
@@ -7,9 +7,11 @@
 Tests for COSE.
 """
 
+from cryptography.hazmat.primitives.asymmetric import ec
 import pytest
 
 from cwt import COSE, COSEKey
+from cwt.algs.ec2 import EC2Key
 
 
 class TestCOSE_HPKE:
@@ -300,3 +302,31 @@ class TestCOSE_HPKE:
         )
         recipient = COSE.new()
         assert b"This is the content." == recipient.decode(encoded, rsk)
+
+    def test_cose_hpke_with_t_cose_example(self):
+
+        pkr_buf = bytes(bytearray([
+            0x04, 0x6d, 0x35, 0xe7, 0xa0, 0x75, 0x42, 0xc1, 0x2c, 0x6d, 0x2a, 0x0d,
+            0x2d, 0x45, 0xa4, 0xe9, 0x46, 0x68, 0x95, 0x27, 0x65, 0xda, 0x9f, 0x68,
+            0xb4, 0x7c, 0x75, 0x5f, 0x38, 0x00, 0xfb, 0x95, 0x85, 0xdd, 0x7d, 0xed,
+            0xa7, 0xdb, 0xfd, 0x2d, 0xf0, 0xd1, 0x2c, 0xf3, 0xcc, 0x3d, 0xb6, 0xa0,
+            0x75, 0xd6, 0xb9, 0x35, 0xa8, 0x2a, 0xac, 0x3c, 0x38, 0xa5, 0xb7, 0xe8,
+            0x62, 0x80, 0x93, 0x84, 0x55
+        ]))
+        pkr = COSEKey.new(EC2Key.to_cose_key(ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP256R1(), pkr_buf)))
+
+        skr_buf = bytes(bytearray([
+            0x37, 0x0b, 0xaf, 0x20, 0x45, 0x17, 0x01, 0xf6, 0x64, 0xe1, 0x28, 0x57,
+            0x4e, 0xb1, 0x7a, 0xd3, 0x5b, 0xdd, 0x96, 0x65, 0x0a, 0xa8, 0xa3, 0xcd,
+            0xbd, 0xd6, 0x6f, 0x57, 0xa8, 0xcc, 0xe8, 0x09
+        ]))
+        params = EC2Key.to_cose_key(ec.derive_private_key(int.from_bytes(skr_buf, byteorder="big"), ec.SECP256R1()))
+        params[2] = b"fixed_test_key_p256r1"
+        skr = COSEKey.new(params)
+
+        msg = bytes.fromhex("d8608443a10101a10550ff96274fe513d6d5556bd5149e954c53582305126370fc3fbfc1d1bc1710d58e6de52be729d30db2497a10f83e52edd29438fb9809818343a10120a2238410010158410403be32ff3820be4f311e0112d2bd58b907d5a33141a5d112f6729669f9f310a11774246487d404b96e32300eb4b18de88d32bb85458cc126fc8a6f2f59ce888e045566697865645f746573745f6b65795f7032353672315820bc7b8684ba8b5170f04fa6feccc338266cfe7b278bd95b1c03bad0023f58f880")
+
+        pt = bytes.fromhex("5468697320697320746865207061796c6f6164")
+
+        recipient = COSE.new()
+        assert pt == recipient.decode(msg, skr)
